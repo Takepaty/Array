c22e3d5326374e02c09aa5324ad7b6e3a833d356
Patricia Takehara <paty.takeharab@outlook.com>
2022-12-06 16:12:47 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace PercorrendoArrayComForForeach
+{
+  class program
+  {
+    public static void Main(string[] args)
+    {
+      int[] arrayInt = new int[5];
+
+      arrayInt[0] = 75;
+      arrayInt[1] = 13;
+      arrayInt[2] = 102;
+      arrayInt[3] = 65;
+      arrayInt[4] = 1;
+
+      Console.WriteLine("Percorrendo o Array com o FOR!");
+      Console.WriteLine();
+
+      for (int contador = 0; contador < arrayInt.Length; contador++)
+      {
+        Console.WriteLine($"Posição Nº {contador} - {arrayInt[contador]}.");
+      }
+      Console.WriteLine("Percorrendo o Array com o FOREACH!");
+      Console.WriteLine();
+
+      int contadorForeach = 0;
+      foreach (int valor in arrayInt)
+      {
+        Console.WriteLine($"Posição Nº {contadorForeach} - {valor}.");
+        contadorForeach++;
+      }
+    }
+  }
+}
+
